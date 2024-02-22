d65403d8b5a7541d17ae1fd8fb9179f6b8623cd6
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-02-23 02:55:16 +0300

    ilk aşama yüklendi

---
+++ boks.java/src/Fighter.java
@@ -0,0 +1,17 @@
+public class Fighter {
+    String name;
+    int damage;
+    int healtyh;
+    int wight;
+    double dodge;
+
+    public Fighter( String name, int damage, int healtyh, int wight, double dodge ) {
+        this.name = name;
+        this.damage = damage;
+        this.healtyh = healtyh;
+        this.wight = wight;
+        this.dodge = dodge;
+    }
+
+
+}
