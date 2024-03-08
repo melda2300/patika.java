26ffb51f0237ba427c50884cb0f90f593af74862
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-03-08 19:46:47 +0300

    yenilendi

---
+++ kurs/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    String name;
+    String lessonCode;
+    String branch;
+
+    public Teacher( String name, String branch, String lessonCode ) {
+        this.name = name;
+        this.branch = branch;
+        this.phoneNumber = phoneNumber;
+    }
+}
